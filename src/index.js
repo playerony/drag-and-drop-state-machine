c0215b8cb2fb50d4669cc9bcfdc5d7525929b65c
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-07-28 22:03:02 +0200

    fix: lint warns

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ const doActions = async () => {
   await fs.createDirectory('files');
   await fs.createDirectory('/files/private');
   await fs.createDirectory('root/files/public');
-
-  console.log(await fs.isDirectory('root')); // true
-  console.log(await fs.isDirectory('files')); // true
-  console.log(await fs.isDirectory('/files/private'));
 };
 
 doActions();
