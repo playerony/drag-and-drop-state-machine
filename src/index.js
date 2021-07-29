9b1a42547b035272c790fd4d60ebd36b9b0096b6
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-07-29 22:39:23 +0200

    🔧 ci: add formatting for html files (#5)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import fs from 'indexeddb-fs';
+import { assign, interpret, createMachine } from 'xstate';
 
 import './style.css';
 
-const doActions = async () => {
-  await fs.createDirectory('files');
-  await fs.createDirectory('/files/private');
-  await fs.createDirectory('root/files/public');
-};
+// const bodyElement = document.body;
+const boxElement = document.getElementById('box');
 
-doActions();
+const assignClickPosition = assign({
+  clickPositionX: (_, event) => event.clientX,
+  clickPositionY: (_, event) => event.clientY,
+});
+
+const dragAndDropMachine = createMachine({
+  initial: 'idle',
+  context: {
+    clickPositionX: 0,
+    clickPositionY: 0,
+  },
+  states: {
+    idle: {
+      on: {
+        mousedown: {
+          target: 'idle',
+          actions: assignClickPosition,
+        },
+      },
+    },
+  },
+});
+
+const service = interpret(dragAndDropMachine);
+
+service.start();
+
+boxElement.addEventListener('mousedown', (event) => {
+  service.send(event);
+});
